77a9eccd1b413a76d7e48a699c32dc1e974dc551
olcaycanitez <olcaycanitez@gmail.com>
2020-10-31 09:42:44 -0700

    no treak or trick

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,6 +1,12 @@
 public class HalloweenTest {
 
     // I am learning git --- GitHub
+    // are we there yet ???
+    // lets play a game
+    // its fun to drive so far
+
+    // there will be no treat and trick
+    // stay home and be safe and wear your mask outsie
 
 
 }
